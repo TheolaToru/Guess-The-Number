b54a61563493732e5b4a2669619592ad631e859e
TheolaToru <145143825+TheolaToru@users.noreply.github.com>
2024-06-21 23:54:40 +0100

    Add files via upload

---
+++ Guess The Number.py
@@ -0,0 +1,29 @@
+from mine import span, number_guesser, play_again
+import math
+from random import choice
+
+
+def main():
+    a, b = span()
+    tries = round(math.log((int(b) - int(a)), 2))
+    correct = choice(range(int(a), int(b)))
+    while tries > 0:
+        print(f"Tries left = {tries}")
+        yours = number_guesser()
+        tries = tries - 1
+        if yours == correct:
+            print("You win!")
+            break
+        elif yours < correct:
+            print("You guessed too low")
+        elif yours > correct:
+            print("You guessed too high")
+    if yours != correct:
+        print("You lost! haha skill issue")
+    again = play_again()
+    if again:
+        main()
+
+
+
+main()
